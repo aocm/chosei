39193f38b00fe1f9d40f911c55972506531cac13
Kazuki Aoshima <kazuki.aoshima@bigtreetc.com>
2020-05-10 13:31:38 +0900

    仮認証機能対応（front側）
    TODO デプロイ時の.envファイルの書き換え

--- front/src/pages/top.vue
+++ front/src/pages/top.vue
@@ -39,7 +39,7 @@
 <script>
 import _ from 'lodash';
 import EvenlyArticle from '../layouts/evenly-article';
-import { chouseiApi } from '../module/api';
+import { chouseiApi, authApi } from '../module/api';
 import { getQuaryDate } from '../module/utilityTools';
 
 export default {
@@ -56,13 +56,47 @@ export default {
       model: null,
     };
   },
-
   mounted() {
     this.initForm();
   },
 
   methods: {
+    // TODO: 認証を全画面に適用するならrouterに組み込む
+    async authCheck() {
+      try {
+        // tokenが正常であれば処理を終了する
+        const isAuthenticated = await this.authenticatedCheck();
+        if (isAuthenticated) return;
+
+        const mydata = { KEY: null };
+        mydata.KEY = await window.prompt('Please input your key!!');
+        const response = await authApi.authentication(mydata);
+        console.log(response.data);
+        if (response.apiStatus && response.apiStatus.value === 'ok') {
+          console.log('OK');
+          localStorage.token = response.data.token;
+        } else {
+          console.log('BAD');
+          throw new Error();
+        }
+      } catch (e) {
+        console.log(e);
+        this.toError401();
+      }
+    },
+    async authenticatedCheck() {
+      if (!localStorage.token) return false;
+      const myToken = { token: localStorage.token };
+      const response = await authApi.checkToken(myToken);
+      return (response.apiStatus && response.apiStatus.value === 'ok');
+    },
+    toError401() {
+      console.log('toError401');
+      this.$router.push({ path: 'auth_error' });
+    },
+
     async initForm() {
+      await this.authCheck();
       this.getUserResponse = await chouseiApi.getUser();
       this.candidateDate = await chouseiApi.getCandidateDate(getQuaryDate());
       const candidateData = {
